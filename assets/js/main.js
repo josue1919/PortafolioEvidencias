5e61b0f92348a4d0100f003f6bad1fd59a8a3020
josue1919 <52299245+josue1919@users.noreply.github.com>
2020-11-29 22:54:48 -0600

    Maquetado

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,8 @@ window.addEventListener('load', () => {
 
     btnSwitch.addEventListener('click', () => {
 	    document.getElementById('barralateral').classList.toggle('dark');
-    document.getElementById('contenedor').classList.toggle('dark');
+    // document.getElementById('contenedor').classList.toggle('dark');
+    document.body.classList.toggle('dark');
     document.getElementById('switch').classList.toggle('active')
 
 	// Guardamos el modo en localstorage.
@@ -20,13 +21,14 @@ window.addEventListener('load', () => {
 // Obtenemos el modo actual.
 if(localStorage.getItem('dark-mode') === 'true'){
     document.getElementById('barralateral').classList.add('dark');
-    document.getElementById('contenedor').classList.add('dark');
-	 
+    document.body.classList.add('dark');
+
     btnSwitch.classList.add('active');
 } else {
     btnSwitch.classList.remove('active');
     document.getElementById('barralateral').classList.remove('dark');
-    document.getElementById('contenedor').classList.remove('dark');
+    document.body.classList.remove('dark');
+
 }
 
 });
